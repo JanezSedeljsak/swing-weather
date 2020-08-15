08d1d95e30c9233820c06ceb60cb4725da0f8add
JanezSedeljsak <janez.sedeljsak@gmail.com>
2020-08-15 23:12:37 +0200

    enter pressed & trying to implement sqlite

--- src/main/java/AppPckg/MainWindow.java
+++ src/main/java/AppPckg/MainWindow.java
@@ -3,18 +3,41 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
 
 public class MainWindow {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception, ClassNotFoundException {
+        SQL.createDB();
         JFrame f = new JFrame();
 
         JLabel searchLabel = new JLabel();
         searchLabel.setText("Išči vreme po kraju");
         searchLabel.setBounds(10,10,500, 20);
 
-        JTextArea searchBar = new JTextArea("");
+        final JTextArea searchBar = new JTextArea("");
         searchBar.setBounds(10,30,500,30);
         searchBar.setBorder(BorderFactory.createLineBorder(Color.black));
+        searchBar.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) { }
+            @Override
+            public void keyReleased(KeyEvent e) { }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // enter pressed
+                if (e.getKeyCode() == 10) {
+                    try {
+                        System.out.println(Api.gWeatherByLocation(searchBar.getText()));
+                        searchBar.setText("");
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+                }
+            }
+        });
 
         JButton historyButton = new JButton("Zgodovina iskanja");
         historyButton.setBounds(10, 70, 500, 30);
--- src/main/java/AppPckg/SQL.java
+++ src/main/java/AppPckg/SQL.java
@@ -1,7 +1,64 @@
 package AppPckg;
 
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class SQL {
+
+    public static void createDB() throws ClassNotFoundException, IOException {
+        //get db file
+        try{
+            File database = new File("database.db");
+            database.createNewFile();
+            System.out.println("create db");
+        }catch (Exception err){
+            Helpers.showMessageDialog(null, "Napaka pri kreiranju baze");
+            //custom error!!!
+        }
+
+        String[] tables = {""};
+        tables[0] = new StringBuilder()
+                .append("CREATE TABLE IF NOT EXISTS `history`")
+                .append("( `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,")
+                .append("`data` TEXT NOT NULL,")
+                .append("`stamp` TEXT NOT NULL)")
+                .toString();
+        //generate tables if they dont exsist
+        try(Connection conn = DriverManager.getConnection("jdbc:sqlite:database.db");
+            Statement stmt = conn.createStatement()) {
+            for(String sql: tables) stmt.execute(sql);
+            System.out.println(tables.toString());
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+            //error on base connection
+        }
+    }
+
     public static Object gSearchById(int id) {
+
+        String sql = new StringBuffer()
+                .append("SELECT * FROM history")
+                .append("WHERE id = '")
+                .append(id)
+                .append("';")
+                .toString();
+
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:database.db");
+             Statement stmt  = conn.createStatement();
+             ResultSet result    = stmt.executeQuery(sql)){
+            if(result.next()) {
+                return "vreme";
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
         return null;
     }
 
