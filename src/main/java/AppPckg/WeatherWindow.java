593b7d9fe23f9c1e8966d2334e358c9572187794
JanezSedeljsak <janez.sedeljsak@gmail.com>
2020-08-17 22:37:36 +0200

    weather component finished

--- src/main/java/AppPckg/WeatherWindow.java
+++ src/main/java/AppPckg/WeatherWindow.java
@@ -26,39 +26,30 @@ public class WeatherWindow {
         data = (JSONObject) parser.parse(weatherResponse);
 
         JLabel locationLabel = new JLabel();
-        locationLabel.setText("Vreme za: " + (String)data.get("name"));
-        locationLabel.setBounds(10,10,500, 20);
+        locationLabel.setText("<html><h1>Vreme za: " + (String)data.get("name") + "</h1></html>");
+        locationLabel.setBounds(10,10,500, 30);
 
         JLabel currentDateLabel = new JLabel();
-        currentDateLabel.setText("Datum: " + java.time.LocalDate.now().toString());
+        currentDateLabel.setText("<html><h3 style='color: #6f6f6f'>Datum: " + java.time.LocalDate.now().toString() + "</h3></html>");
         currentDateLabel.setBounds(10,40,500, 20);
 
         JLabel weatherData = new JLabel();
         weatherData.setText(
-                "Temperatura: " + String.valueOf(((JSONObject)data.get("main")).get("temp")) + "\n"
-                + "Veter: " + String.valueOf(((JSONObject)data.get("wind")).get("speed")) + "km/h \n"
+                "<html><ul>"
+                + "<li>Vreme: " + String.valueOf(((JSONObject)((JSONArray)data.get("weather")).get(0)).get("main")) + "</li>\n"
+                + "<li>Temperatura: " + String.valueOf(((JSONObject)data.get("main")).get("temp")) + "</li>\n"
+                + "<li>Veter: " + String.valueOf(((JSONObject)data.get("wind")).get("speed")) + "km/h </li>\n"
+                + "<li>Vlaga: " + String.valueOf(((JSONObject)data.get("main")).get("humidity")) + "% </li>\n"
+                + "</ul></html>"
         );
-        weatherData.setBounds(10, 70, 500, 50);
-
-        JSONArray forcast = (JSONArray) data.get("weather");
-
-
-
-
-
-        String week[]= { "Monday","Tuesday","Wednesday",
-                "Thursday","Friday","Saturday","Sunday"};
-
-        JList forecast = new JList(week);
-        forecast.setBounds(10,150, 520, 300);
-
+        weatherData.setBounds(10, 70, 500, 80);
 
 
         f.add(locationLabel);
         f.add(currentDateLabel);
         f.add(weatherData);
 
-        f.setSize(540,800);
+        f.setSize(540, 200);
         f.setLayout(null);
         f.setVisible(true);
         f.setTitle("Vreme");
